81bcf82b6eba269bb324cdef65d30c0849bd3b62
Artmiss G <artmiss1380@gmail.com>
2024-07-18 19:10:14 +0330

    encapsulation of search engine

--- src/llm.py
+++ src/llm.py
@@ -5,22 +5,21 @@ from groq import Groq
 # loading variables from .env file
 load_dotenv() 
 
-print(os.environ.get("GROQ_API_KEY"))
-
 client = Groq(
     # This is the default and can be omitted
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-prompt = input("Prompt: ")
+if __name__ == "__main__" :
+    prompt = input("Prompt: ")
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": prompt,
-        }
-    ],
-    model="llama3-8b-8192",
-)
-print(chat_completion.choices[0].message.content)
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+    print(chat_completion.choices[0].message.content)
--- src/search_engine.py
+++ src/search_engine.py
@@ -1,80 +1,73 @@
 from pathlib import Path
 import json
 
-from whoosh.index import create_in
+from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, TEXT, ID
 from whoosh.qparser import QueryParser, MultifieldParser
 from whoosh.qparser import FuzzyTermPlugin, PrefixPlugin
-from whoosh.index import open_dir
+
+class SearchEngine:
+    def __init__(self, index_dir, doc_dir):
+        self.index_dir = Path(index_dir)
+        self.doc_dir = Path(doc_dir)
+        self.index = None
+        with open(self.doc_dir, 'r') as f:
+             self.documents = json.load(f)
+
+    def create_index(self):
+        self.index_dir.mkdir(exist_ok=True)
+        # re-open docs, since it might have been modified
+        with open(self.doc_dir, 'r') as f:
+            self.documents = json.load(f)
+
+        schema = Schema(
+            doc_id=ID(stored=True, unique=True),
+            title=TEXT(stored=True),
+            content=TEXT,
+        )
+        self.index = create_in(self.index_dir, schema)
+
+        writer = self.index.writer()
+        for doc in self.documents:
+            writer.add_document(doc_id=doc['doc_id'], title=doc['title'], content=doc['content'])
+        writer.commit()
+
+    def load_index(self):
+        self.index = open_dir(self.index_dir)
+
+    def create_query_parser(self):
+        parser = MultifieldParser(["title", "content"], schema=self.index.schema)
+        parser.add_plugin(FuzzyTermPlugin())
+        parser.add_plugin(PrefixPlugin())
+        return parser
+
+    def search_index(self, query_str):
+        parser = self.create_query_parser()
+        query = parser.parse(query_str)
+        results_list = []
+        with self.index.searcher() as searcher:
+            results = searcher.search(query, terms=True)
+            for hit in results:
+                results_list.append(hit.fields())
+        return results_list
 
 
-def create_index(index_dir, doc_dir):
-    index_dir.mkdir(exist_ok=True)
+    def __call__(self):
+        if not self.index_dir.exists():
+            self.create_index()
+        else:
+            self.load_index()
 
-    with open(doc_dir, 'r') as f:
-        documents = json.load(f)
+        query_str = input("Enter your search query: ")
+        results_list = self.search_index(query_str)
 
-    schema = Schema(
-        doc_id=ID(stored=True, unique=True),
-        title=TEXT(stored=True),
-        content=TEXT,
-    )
-    index = create_in(index_dir, schema)
-
-    writer = index.writer()
-    for doc in documents:
-        writer.add_document(doc_id=doc['doc_id'], title=doc['title'], content=doc['content'])
-    writer.commit()
-
-    return index
-
-def load_index(index_dir):
-    index = open_dir(index_dir)
-    return index
-
-def create_query_parser(index):
-    parser = MultifieldParser(["title", "content"], schema=index.schema)
-    # parser = QueryParser("title", index.schema)
-    parser.add_plugin(FuzzyTermPlugin())
-    parser.add_plugin(PrefixPlugin())
-    return parser
-
-
-def search_index(index, query_str):
-    parser = create_query_parser(index)
-    query = parser.parse(query_str)
-    results_list = []
-    with index.searcher() as searcher:
-        results = searcher.search(query, terms=True)
-        for hit in results:
-            results_list.append(hit.fields())
-
-    return results_list
-
-
-def print_search_results(index, results_list, documents):
-    for hit in results_list:
-        print(hit["title"], ":")
-        print(documents[int(hit['doc_id'])]['content'])
-
-
-def main():
-    index_dir = Path('./indexes')
-    doc_dir = Path('./data/documents.json')
-
-    if index_dir.exists() :
-        index = load_index(index_dir)
-    else :
-        index = create_index(index_dir, doc_dir)
-
-    query_str = input("Enter your search query: ")
-    results_list = search_index(index, query_str)
-
-    with open(doc_dir, 'r') as f:
-        documents = json.load(f)
-
-    print_search_results(index, results_list, documents)
+        for hit in results_list:
+            yield {
+                'title': hit["title"],
+                'content': self.documents[int(hit['doc_id'])]['content']
+            }
 
 
 if __name__ == '__main__':
-    main()
+    search_engine = SearchEngine('./indexes', './data/documents.json')
+    print(*list(search_engine()))
